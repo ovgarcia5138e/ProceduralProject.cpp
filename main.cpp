cf66f5c3d5edfc1ca966b61a5fbcd41f88ab16c0
ovgarcia <ovgarcia@github.com>
2019-05-15 11:30:18 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Hello world program
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Begining of program " << flush;
+    cout << "Hello World" << endl;
+    cout << "Another " << "Text " << "I guess " << endl;
+    cout << "Trying more text" << endl;
+
+
+    return 0;
+}
