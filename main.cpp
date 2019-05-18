82703b919b3468e9b63bdcdc8ffc813169781679
ovgarcia <ovgarcia@github.com>
2019-05-18 17:26:30 -0400

    Testing command line commit

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main() {
       cout << "3. Add Movie Player\n";
       cout << "4. Display Production Statistics\n";
       cout << "5. Exit\n";
+      cout << "Testing command line push";
 
     return 0;
 }
