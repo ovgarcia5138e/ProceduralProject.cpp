37fe7551fd48e6d7a9d24117d0c7a91d316783b9
ovgarcia5138e <36053669+ovgarcia5138e@users.noreply.github.com>
2019-05-24 18:46:20 -0400

    Update main.cpp
    Updated program

--- main.cpp
+++ main.cpp
@@ -1,37 +1,56 @@
-//Hello world program
+/*
+Hello world program
+@file main.cpp
+@brief The majority of a media player production program.
 
+
+main.cpp is the primary file for the media player production program.
+The program was created in COP 2001 in the summer of 2019.
+
+
+ @author Oscar Garcia Vazquez
+ @bug no known bugs
+
+*/
 
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <cstdlib>
+
 
 using namespace std;
 
+
 // Prototype functions.
 int menuOptions(int menuinputone);
+
 void showMenu();
+
 void introductionQuestion(string firstescapeattempt);
+
 int menuOptionstonextmenu(int firstmenuinput);
+
 void appleCompayProducts(string manufacturer);
+
 
 int main() {
 
-    ofstream myfile;
-    myfile.open("ProductList.txt");
-    myfile << "Hello World! \n";
-    myfile.close();
 
     cout << "Lets get Started enter your name: " << endl;
 
+
+    // this is the username of the person using the program
     string username;
 
     cin >> username;
 
-    cout << "Hey " << username << " you managed to hack into Cisco Corportation system, this is an AI speaking, my name is not inmortant"
-                                  " for now refer to me as tosh togther we will "
-                                  "take what we need and use it to our advantage." << endl;
+    cout << "Hey " << username
+         << " you managed to hack into Cisco Corportation system, this is an AI speaking, my name is not inmortant"
+            " for now refer to me as tosh togther we will "
+            "take what we need and use it to our advantage." << endl;
 
-    cout <<"\n";
+    cout << "\n";
 
     cout << "If you want to back out enter escape: " << endl;
 
@@ -43,7 +62,7 @@ int main() {
 
     cout << "\n";
     cout << "Pick on of  the options below by inputing a value 1-6:"
-            << endl;
+         << endl;
 
     // showMenu() displays the home menu of the production line.
     showMenu();
@@ -51,34 +70,82 @@ int main() {
     int firstmenuinput;
     cin >> firstmenuinput;
 
-
+    //Function that demonstrates sub menus that the original menu displays.
     menuOptionstonextmenu(firstmenuinput);
 
-    // Eventually the user will be able to choose the item to produce.
-    // For now, just have them input the information.
+    // The input for the company manufacturer.
     cout << "Enter the manufacturer\n";
     string manufacturer;
     cin >> manufacturer;
 
+
+    // This method produces the products that the company Apple has.
     appleCompayProducts(manufacturer);
 
     int itemTypeChoice;
     cin >> itemTypeChoice;
 
     string itemTypeCode;
-    // write code to set the item type code based on the selected item type
-    // Audio "MM", Visual "VI", AudioMobile "AM", or VisualMobile "VM".
 
 
+    /*
+    struct student {
+        char newproduct[50];
+        int roll;
+        //float marks;
+    } s[10];
 
+    for (int i = 0; i < 3; ++i) {
+        s[i].roll = i + 1;
+        cout << "For product  " << s[i].roll << "," << endl;
+
+        cout << "Enter product : ";
+        cin >> s[i].newproduct;
+
+        cout << endl;
+    }
+
+    cout << "Displayign Info : " << endl;
+
+    for (int i = 0; i < 3; ++i) {
+        cout << "\nProduct serial number: " << i + 1 << endl;
+        cout << "Product name: " << s[i].newproduct << endl;
+        //cout << "Marks: " << s[i].marks << endl;
+
+    }
+
+    list<int> mylist;
+    list<int>:: iterator it;
+
+    for (int i =1; i <=5; ++i) mylist.push_back(i);
+
+    it = mylist.begin();
+    ++it;
+
+    mylist.insert(it, 22);
+
+    mylist.insert(it,20);
+
+    --it;
+
+    vector<int> myvector (2,30);
+    mylist.insert(it,myvector.begin(), myvector.end());
+
+    cout << "Availible Products: ";
+    for (it=mylist.begin(); it!=mylist.end(); ++it)
+        cout << '\n' << *it;
+    cout <<'\n';
+    */
     return 0;
 }
 
 // functions for program.
 
+
+// Provides sub menus for the main menu
 int menuOptions(int menuinputone) {
 
-    if (menuinputone == 1 ) {
+    if (menuinputone == 1) {
         cout << "Produce Items Stub\n";
         cout << "1. Produce items\n";
         cout << "2. Add Employee Account\n";
@@ -148,6 +215,8 @@ int menuOptions(int menuinputone) {
     return 0;
 }
 
+
+// Shows the default menu.
 void showMenu() {
     cout << "\n";
     cout << "Production Line Tracker\n";
@@ -160,16 +229,22 @@ void showMenu() {
 
 }
 
-void introductionQuestion(string firstescapeattempt){
+
+// Input for the introduction question.
+void introductionQuestion(string firstescapeattempt) {
     if (firstescapeattempt == "escape") {
-        cout << "You made the mistake of entering your name you must do as I say or I will frame you. Lets make better decisions since we are working togher\"\n"
-                "                \"now partner. " << endl;
+        cout
+                << "You made the mistake of entering your name you must do as I say or I will frame you. Lets make better decisions since we are working togher\"\n"
+                   "                \"now partner. " << endl;
     } else {
-        cout << "You're not very good at following directions are you? Lets make better decisions since we are working togher"
-                "now partner." << endl;
+        cout
+                << "You're not very good at following directions are you? Lets make better decisions since we are working togher"
+                   "now partner." << endl;
     }
 }
 
+
+// Provides the pathway to the next menu stubs from the main menu.
 int menuOptionstonextmenu(int firstmenuinput) {
     do {
         if (firstmenuinput >= 1 && firstmenuinput <= 5) {
@@ -181,7 +256,7 @@ int menuOptionstonextmenu(int firstmenuinput) {
 
             menuOptions(thirdmenuinput);
 
-            if (thirdmenuinput == 6){
+            if (thirdmenuinput == 6) {
                 //menuOptions(6);
                 //return 0;
                 break;
@@ -203,24 +278,28 @@ int menuOptionstonextmenu(int firstmenuinput) {
             return 0;
         }
 
-    } while(firstmenuinput >=1 && firstmenuinput < 6);
+    } while (firstmenuinput >= 1 && firstmenuinput < 6);
 
 }
 
-void appleCompayProducts(string manufacturer){
 
-    if  (manufacturer == "Apple"){
+// Displays the products that the Apple company has, tracks, and saves the quantity made and gives
+// a custom serial number for each product
+void appleCompayProducts(string manufacturer) {
+
+    if (manufacturer == "Apple") {
         cout <<
              "Apple Products\n"
              << "1. iPod\n"
-             <<"2. iPhone\n"
-             <<"3. i Pad\n"
-             <<"4. Ear Pods\n";
+             << "2. iPhone\n"
+             << "3. i Pad\n"
+             << "4. Ear Pods\n";
         cout << "Enter the product name\n";
         string prodName;
         cin >> prodName;
 
-        if (prodName =="iPod") {
+
+        if (prodName == "iPod") {
             cout << "Enter the item type\n";
             cout << "1. Audio\n" <<
                  "2. Visual\n" <<
@@ -234,53 +313,87 @@ void appleCompayProducts(string manufacturer){
             // Audio "MM", Visual "VI", AudioMobile "AM", or VisualMobile "VM".
 
 
-            if (itemTypeChoice == 1){
+            if (itemTypeChoice == 1) {
 
                 cout << "Enter the number of items that were produced\n";
                 int numProducedMM;
                 cin >> numProducedMM;
 
-                ofstream productlists;
-                productlists.open("ProductList.txt", ofstream::app);
+                ofstream productlist;
+                productlist.open("ProductList.txt", ios::app);
+
                 for (int start = 1; start <= numProducedMM; start++) {
                     //ofstream myfile ("ProductList.txt", ios::in | ios::out | ios::app);
 
-                    if (productlists.is_open()){
-                        productlists << "\nProduction Number: " << start << " Serial Number: AppMM " << setfill('0') << setw(5) << start;
+                    //if (productlists.is
+                    // _open()){
+                    productlist << "Production Number: " << start << " Serial Number: AppMM " << setfill('0') << setw(5)
+                                << start << endl;
 
-                    } else {
-                        cout << "Unable to open file.\n";
-                    }
+                    //} else {
+                    //cout << "Unable to open file.\n";
+                    // }
                 }
 
-            } else if (itemTypeChoice ==2) {
+                productlist.close();
+
+            } else if (itemTypeChoice == 2) {
                 cout << "Enter the number of items that were produced\n";
                 int numProducedVI;
                 cin >> numProducedVI;
+
+                ofstream producttwo;
+                producttwo.open("ProductList.txt", ios::app);
+
                 for (int start = 1; start <= numProducedVI; start++) {
 
-                    cout << "Production Number: " << start << " Serial Number: AppVI " << setfill('0') << setw(5) << start << endl;
+                    producttwo << "Production Number: " << start << " Serial Number: AppVI " << setfill('0') << setw(5)
+                               << start << endl;
                 }
+
+                producttwo.close();
             } else if (itemTypeChoice == 3) {
                 cout << "Enter the number of items that were produced\n";
                 int numProducedAM;
                 cin >> numProducedAM;
+
+                ofstream productthree;
+                productthree.open("ProductList.txt", ios::app);
+
                 for (int start = 1; start <= numProducedAM; start++) {
 
-                    cout << "Production Number: " << start << " Serial Number: AppAM " << setfill('0') << setw(5) << start << endl;
+                    productthree << "Production Number: " << start << " Serial Number: AppAM " << setfill('0')
+                                 << setw(5) << start << endl;
                 }
             } else if (itemTypeChoice == 4) {
                 cout << "Enter the number of items that were produced\n";
                 int numProducedVM;
                 cin >> numProducedVM;
+
+                ofstream productfour;
+                productfour.open("ProductList.txt", ios::app);
+
                 for (int start = 1; start <= numProducedVM; start++) {
 
-                    cout << "Production Number: " << start << " Serial Number: AppVM" << setfill('0') << setw(5) << start << endl;
+                    productfour << "Production Number: " << start << " Serial Number: AppVM" << setfill('0') << setw(5)
+                                << start << endl;
                 }
             }
         }
 
     }
 
-}
+    cout << "Please type another company if finished enter done";
+    string productmenuagain;
+    cin >> productmenuagain;
 
+    if (productmenuagain == "done") {
+        showMenu();
+    } else {
+        appleCompayProducts(productmenuagain);
+        cout << "\n";
+        cout << "Enter a new product to add: \n";
+
+
+    }
+}
